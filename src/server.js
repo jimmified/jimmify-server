27ed472c90ce182a566f16cae74948b6b50c66b0
Adam Dierkens <adam@dierkens.com>
2016-11-01 22:28:59 -0700

    Add basic koa server and logging

--- src/server.js
+++ src/server.js
@@ -1,3 +1,14 @@
-export default function() {
-  console.log('test');
-}
+import koa from 'koa';
+import log from './log';
+
+const PORT = process.env.PORT || 8888;
+
+const app = koa();
+
+app.use(function *(next) {
+  log.info('%s - %s', this.method, this.url);
+  yield next;
+});
+
+log.info(`Listening on port ${PORT}`)
+app.listen(PORT);
