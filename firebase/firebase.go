bb2e2d637f74e1dc9b055ef50e2075bf5bc54bf6
James Michael Reilly <jmr8893@rit.edu>
2017-01-15 16:43:43 -0800

    Working out bugs, now pushes using device ids

--- firebase/firebase.go
+++ firebase/firebase.go
@@ -9,21 +9,32 @@ import (
 var serverKey string
 var topic string
 
-func init() {
+//Init : Sets the firebase key and topic from env vars
+func Init() {
 	serverKey = os.Getenv("JFIREBASEKEY")
-	topic = os.Getenv("JFIREBASETOPIC")
+	//topic = os.Getenv("JFIREBASETOPIC")
 }
 
-func push(msg, sum string) {
+//Push : Sends a push notification using Google Cloud Messaging
+//msg, The message to be sent
+//sum, The title of the notification
+func Push(msg, sum string) {
 	data := map[string]string{
 		"msg": msg,
 		"sum": sum,
 	}
 
-	c := fcm.NewFcmClient(serverKey)
-	c.NewFcmMsgTo(topic, data)
+	ids := []string{
+		"dW3SHCVmDsI:APA91bE-MXR-ynBpoR-CeWUrWoinA_WZE9_WjEzrgxKewlEF2r_noo840EkR-XkkoCB0FUcgAS3E96GeIFTiZUoxFVXRaALUOxE-6hLVdpS_h6HOBjfwpnwtTToLG3uRQ3HX9JAWxOB-",
+	}
 
+	c := fcm.NewFcmClient(serverKey)
+	//c.NewFcmMsgTo(topic, data)
+	c.NewFcmRegIdsMsg(ids, data)
 	status, err := c.Send()
+
+	fmt.Println(serverKey)
+	fmt.Println(topic)
 
 	if err == nil {
 		status.PrintResults()
